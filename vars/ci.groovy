ee4f9f332c61942bb5c3fa65f6b00552c5b8c816
Nithin Perala <nitin.perala@gmail.com>
2020-08-12 21:53:48 -0700

    checkout

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -2,22 +2,26 @@
 def call(Map pipelineParams)
 {
         node {
-        def mvnHome
-        stage('Preparation') { // for display purposes
-            // Get some code from a GitHub repository
-        //git 'https://github.com/nitinperala/RunnableJar.git'
-            def chckOut = new cipull()
-            chckOut.checkOutFrom(pipelineParams.repo)
+            def mvnHome
+            stage('Preparation') { // for display purposes
+                // Get some code from a GitHub repository
+            //git 'https://github.com/nitinperala/RunnableJar.git'
+                def chckOut = new cipull()
+                chckOut.checkOutFrom(pipelineParams.repo)
+                
+                // Get the Maven tool.
+                // ** NOTE: This 'M3' Maven tool must be configured
+                // **       in the global configuration.
+                mvnHome = tool 'Maven'
+            }
+            stage('Build') {
+            // Run the maven build
+            new mvnbuild().buildcmd(pipelineParams.mvnbuild,mvnHome)
             
-            // Get the Maven tool.
-            // ** NOTE: This 'M3' Maven tool must be configured
-            // **       in the global configuration.
-            mvnHome = tool 'Maven'
         }
-         stage('Build') {
-        // Run the maven build
-        new mvnbuild().buildcmd(pipelineParams.mvnbuild,mvnHome)
-        
-    }
+        stage('Results') {
+            
+            archiveArtifacts 'target/*.jar'
+        }
     }
 }
