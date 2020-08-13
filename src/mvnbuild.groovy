48cdad039666be79efcc9cfbdee268b733db7b48
Nithin Perala <nitin.perala@gmail.com>
2020-08-12 21:49:31 -0700

    checkout

--- src/mvnbuild.groovy
+++ src/mvnbuild.groovy
@@ -1,12 +1,12 @@
- #!/usr/bin/groovy
-def call(mvncmd,mvnHome)
+#!/usr/bin/groovy
+def buildcmd(mvncmd,mvnHome)
 {
     
-    withEnv(["MVN_HOME=${mvnHome}"]) {
+    
                 if (isUnix()) {
                     sh '"${mvnHome}/bin/mvn" -Dmaven.test.failure.ignore ${mvncmd}'
                 } else {
                     bat(/"%${mvnHome}%\bin\mvn" -Dmaven.test.failure.ignore ${mvncmd}/)
                 }
-    }
+    
 }
