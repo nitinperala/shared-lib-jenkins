d76433f17df066755f22b1f330b9aa245540833f
Nithin Perala <nitin.perala@gmail.com>
2020-08-12 21:17:44 -0700

    checkout

---
+++ vars/ci.groovy
@@ -0,0 +1,16 @@
+def call(Map pipelineParams)
+{
+    node {
+    def mvnHome
+    stage('Preparation') { // for display purposes
+        // Get some code from a GitHub repository
+       //git 'https://github.com/nitinperala/RunnableJar.git'
+        def chckOut = new cipull()
+        chckOut.checkOutFrom('RunnableJar.git')
+        
+        // Get the Maven tool.
+        // ** NOTE: This 'M3' Maven tool must be configured
+        // **       in the global configuration.
+        mvnHome = tool 'Maven'
+    }
+}
