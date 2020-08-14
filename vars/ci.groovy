463b667b4153da584c17532c0e23141d6eb959d5
Nithin Perala <nitin.perala@gmail.com>
2020-08-13 21:47:19 -0700

    Adding closures to vars files

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -8,7 +8,7 @@ def call(body)
         node {
             def mvnHome
             env.name = "nithin"
-            pipelineParams[branch] = 'master'
+            pipelineParams['branch'] = 'master'
             stage('Preparation') { // for display purposes
                 // Get some code from a GitHub repository
             //git 'https://github.com/nitinperala/RunnableJar.git'
